2d6b2a7173db558565558891a1be3a73efb6a7f8
ArunSonvani <arunsonvani@gmail.com>
2022-07-05 13:18:56 +0530

    sunny asp

---
+++ lib/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("home"),
+        centerTitle: true,
+      ),
+      body: Column(children: [
+        Text("hello Sunny"),
+        Text("hello Arun kumar"),
+      ]),
+    );
+  }
+}
